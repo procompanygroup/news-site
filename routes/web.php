1467e1c9817eb5aa9dcbc8c1ff7c6c60f807e85e
ahmadms2020 <70610331+ahmadms2020@users.noreply.github.com>
2023-12-31 16:17:34 +0200

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/{page}', [AdminController::class, 'index']);
+//Route::get('/{page}', [AdminController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
